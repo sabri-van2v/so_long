56f869436aac00ebcd8143d037f73a97fa778106
Sabri VAN DE VELDE <ssaaa.ele@gmail.com>
2023-01-29 02:28:30 +0100

    .xpm printed and start mlx doc by aurelien brabant

--- so-long/srcs/test.c
+++ so-long/srcs/test.c
@@ -21,13 +21,18 @@ int main()
 	void *mlx;
 	t_data img;
 	void *mlx_windows;
+	void *test_img;
+	int	img_width;
+	int	img_height;
 
 	mlx = mlx_init();
 	mlx_windows = mlx_new_window(mlx, 800, 800, "letsgooooo");
 	img.img = mlx_new_image(mlx, 800, 800);
 	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
 								&img.endian);
+	test_img = mlx_xpm_file_to_image(mlx, "./img/bastien64.xpm", &img_width, &img_height);
 	my_mlx_pixel_put(&img, 100, 100, 0x00FF0000);
-	mlx_put_image_to_window(mlx, mlx_windows, img.img, 0, 0);
-	  mlx_loop(mlx);
+	mlx_put_image_to_window(mlx, mlx_windows, test_img, 0, 0);
+	//mlx_put_image_to_window(mlx, mlx_windows, test_img, 20, 0);
+	mlx_loop(mlx);
 }
